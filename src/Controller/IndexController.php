eaec2bfd399214d67eca43c9ce60a5c4ca84a4d8
Gridlock242 <Neltharion@live.fr>
2025-05-14 12:22:56 +0200

    Sélecteur de genre modifié

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GenreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +10,15 @@ use Symfony\Component\Routing\Attribute\Route;
 final class IndexController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(int $page = 1): Response
+    public function index(GenreRepository $genreRepository, int $page = 1): Response
     {
-        return $this->render('index/index.html.twig', ['page' => $page]);
+        $genres = $genreRepository->findBy([], ['name' => 'ASC']);
+
+        return $this->render('index/index.html.twig', [
+            'page' => $page,
+            'genres' => $genres,
+        ]);
     }
 }
+
  
