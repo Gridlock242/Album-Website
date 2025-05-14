2c9d00d9a8022b1046cf74ee452bda2fd79090c5
Gridlock242 <Neltharion@live.fr>
2025-05-14 14:28:31 +0200

    Index modifié pour faire apparaître les albums

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\AlbumRepository;
 use App\Repository\GenreRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,13 +11,15 @@ use Symfony\Component\Routing\Attribute\Route;
 final class IndexController extends AbstractController
 {
     #[Route('/', name: 'index')]
-    public function index(GenreRepository $genreRepository, int $page = 1): Response
+    public function index(GenreRepository $genreRepository, AlbumRepository $albumRepository, int $page = 1): Response
     {
         $genres = $genreRepository->findBy([], ['name' => 'ASC']);
+        $albums = $albumRepository->findAll();
 
         return $this->render('index/index.html.twig', [
             'page' => $page,
             'genres' => $genres,
+            'albums' => $albums,
         ]);
     }
 }
