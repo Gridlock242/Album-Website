4cbf0bf4d193888089d4a49dfcdabebf9cb6b7c0
Miles280 <loughsala.imed@gmail.com>
2025-05-17 21:01:10 +0200

    correctif login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,8 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\BrowserKit\Request;
-use Symfony\Component\HttpFoundation\Request as HttpFoundationRequest;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
@@ -12,10 +11,9 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils, HttpFoundationRequest $request): Response
+    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
     {
         // get the login error if there is one
-        $request->getSession()->clear();
         $error = $authenticationUtils->getLastAuthenticationError();
 
         // last username entered by the user
