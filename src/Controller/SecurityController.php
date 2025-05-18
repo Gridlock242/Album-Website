bd1f3c90e4fb55552b5b5a21ddb5e2749f78c262
Gridlock242 <Neltharion@live.fr>
2025-05-18 19:39:39 +0200

    Modification avec erreur à checker

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,16 +2,20 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     #[Route(path: '/login', name: 'app_login')]
-    public function login(AuthenticationUtils $authenticationUtils, Request $request): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
@@ -34,11 +38,34 @@ class SecurityController extends AbstractController
     }
 
 
-    #[Route('/admin', name: 'app_registration')]
-    public function index(): Response
-    {
+    #[Route('/registration', name: 'app_registration')]
+    public function registration(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+{
+    if (headers_sent($file, $line)) {
+        throw new \RuntimeException("⚠️ Les headers ont déjà été envoyés dans le fichier : $file à la ligne : $line");
+    }
+
+        $user = new User();
+        $form = $this->createForm(RegistrationFormType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $hashedPassword = $passwordHasher->hashPassword(
+                $user,
+                $form->get('plainPassword')->getData()
+            );
+            $user->setPassword($hashedPassword);
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre compte a été créé avec succès !');
+
+            return $this->redirectToRoute('app_login');
+        }
+
         return $this->render('security/registration.html.twig', [
-            'controller_name' => 'SecurityController',
+            'registrationForm' => $form->createView(),
         ]);
     }
 }
